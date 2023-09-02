5d1c75cff612287b356868277e4b30fea7b15f82
Saima Binte Faruk <94638018+saimabintefaruk@users.noreply.github.com>
2023-09-02 17:52:01 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function textChange(){
+
+document.getElementById('a').innerHTML="Hello World";
+
+}
+
+//sit-danger
+function textC(){
+  document.getElementById('b').innerHTML="Hello World";
+}
+
+//world-change
+function textCh(){
+
+document.getElementById('c').innerHTML="Hello World";
+
+}
